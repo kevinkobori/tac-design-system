763154ce1a8564402011f7bb36a3b32cc7062644
kevin.kobori <kevin.kobori@darwinseguros.com>
2023-05-17 11:36:56 -0300

    first commit

---
+++ lib/tac_design_system.dart
@@ -0,0 +1,4 @@
+library tac_design_system;
+
+/// A Design System for Tópicos Avançados de Computação - CEUB.
+class TacDesignSystem {}
---
+++ test/tac_design_system_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+// import 'package:tac_design_system/tac_design_system.dart';
+
+void main() {
+  test('', () {});
+}
